d426862c29e49ddc1f58afd7b903e3fe31031a0d
patricia <prmonday@gmail.com>
2021-02-20 08:13:32 +0000

    top_up method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,2 +1,13 @@
+require 'oystercard'
 describe Oystercard do
-end 
+  it ' has a balance'do
+    oystercard = Oystercard.new
+    expect(oystercard.balance).to eq(0)
+  end
+
+  it 'adds money in the card' do
+  oystercard = Oystercard.new
+  oystercard.top_up(3)
+  expect(oystercard.top_up(3)).to eq(1)
+end
+end
