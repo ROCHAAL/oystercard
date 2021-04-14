382e1f39f600acc5a7f77c7b5a3d52ddac57eaf9
patricia <prmonday@gmail.com>
2021-04-14 09:21:29 +0100

    start and current_journey method

--- lib/journey_log.rb
+++ lib/journey_log.rb
@@ -1,7 +1,22 @@
 class JourneyLog
 
-  def start
-    1
+attr_reader :journey_class, :journeys, :entry_station, :station, :current_journey
+  def initialze(journey_class)
+    @journey_class = journey_class
+    @journeys = []
+    @entry_station = entry_station
+    @station = station
+    @current_journey = current_journey
+  end
+
+  def start(entry_station)
+  p  @station
+  end
+
+  private
+
+  def current_journey
+    @current_journey
   end
 
 end
--- spec/journey_log_spec.rb
+++ spec/journey_log_spec.rb
@@ -1,8 +1,20 @@
 require 'journey_log'
 
+
 describe 'JourneyLog' do
+  let(:journey_class) {double :journey}
+  let(:journeys) {double :journey}
+  let(:entry_station) {double :journey}
+  let(:station)  { double :station }
+  let(:current_journey) {double:station}
+
 it 'starts a journey'do
 journey_log = JourneyLog.new
-expect(journey_log.start).to eq(1)
+expect(journey_log.start(entry_station)).to eq([entry_station])
+end
+
+it 'returns an incomplete journey'do
+journey_log = JourneyLog.new
+expect(journey_log).to respond_to(:current_journey)
 end
 end
